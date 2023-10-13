6c4f1f111770e9a8ed5bd502e0dede14bf122b55
Nirej <purampandey1234@gmail.com>
2023-10-13 17:58:16 +0700

    Changes on js

---
+++ scripts/word-list.js
@@ -0,0 +1,37 @@
+const wordList = [
+    {
+        word: "sunflower",
+        hint: "A bright, yellow flower that follows the sun."
+    },
+    {
+        word: "elephant",
+        hint: "The largest land animal with a long trunk."
+    },
+    {
+        word: "computer",
+        hint: "A device for processing and storing information."
+    },
+    {
+        word: "penguin",
+        hint: "A flightless bird found in the Southern Hemisphere."
+    },
+    {
+        word: "chocolate",
+        hint: "A sweet, brown treat made from cocoa beans."
+    },
+    {
+        word: "umbrella",
+        hint: "A device used to shield from rain or sun."
+    },
+    {
+        word:"guiter",
+        hint: "A musical instrument with strings."
+    },
+    {
+        word: "oxygen",
+        hint: "A colorless, odorless gas essential for life."
+    },
+    {    word:"mountain",
+         hint:"A large natural elevation of the Earth's surface."
+    }
+];
